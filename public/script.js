de41de680773d067e294ea845e1877c20b5229c7
DESKTOP-08GHM38\82107 <yskim@tmonet.co.kr>
2022-03-10 19:45:14 +0900

    commit

--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
-window.addEventListener('messsage', function (e) {
+function termsPop() {
+    const modal = document.querySelector('.modal');
+
+    modal.style.display === 'none'
+        ? (modal.style.display = '')
+        : (modal.style.display = 'none');
+}
+
+window.addEventListener('message', function (e) {
     console.log(e.data);
+    const modal = document.querySelector('.modal');
+
+    modal.style.display === 'none'
+        ? (modal.style.display = '')
+        : (modal.style.display = 'none');
 });
